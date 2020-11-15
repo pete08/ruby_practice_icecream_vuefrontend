c9c415acc959990a3c04e0c4571a5834a49ddafa
PeterH <pete08@gmail.com>
2020-11-15 11:27:08 -0600

    Iinitial commit. Add IceCream index to Home.vue using axios get request

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <br>
+    <h1>All Flavors of IceCream for your Enjoyment!</h1>
+    <div v-for="flavor in flavors">
+      <ul>
+        <li>title: {{flavor.title}}</li>
+        <li>{{flavor.description}}</li>
+        <li>price!: {{flavor.price}}</li>
+        <hr>
+      </ul>
+    </div>
+
+
   </div>
+
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      flavors: []
+    };
+  },
+  
+  created: function() {
+    this.indexFlavors();
+  },
+
+
+  methods: {
+    indexFlavors: function() {
+      axios.get("/api/flavors").then(response => {
+        console.log("flavors index: ", response);
+        this.flavors = response.data;
+      });
+    },
+  },
+};
 </script>
